6d23c17f376494087c66d738d97394b2e16be19e
Dan Hunter <dan.hunter55@yahoo.com>
2018-10-25 10:01:26 +0100

    refined code to inlcude an array and a loop

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -26,12 +26,14 @@
 // }
 
 
-var yOne = 20
-var yTwo = 100
-var yThree = 180
-var speedOne = 3
-var speedTwo = 3
-var speedThree = 3
+// var yOne = 20
+// var yTwo = 100
+// var yThree = 180
+var ys = [20, 100, 180]
+// var speedOne = 3
+// var speedTwo = 3
+// var speedThree = 3
+var speeds = [3, 3, 3]
 
 function setup() {
   createCanvas(400, 400)
@@ -41,23 +43,30 @@ function setup() {
 function draw() {
   background(200)
 
-  yOne = yOne + speedOne
-  if (yOne < 0 || yOne > height) {
-    speedOne = speedOne * -1
+  // yOne = yOne + speedOne
+  // if (yOne < 0 || yOne > height) {
+  //   speedOne = speedOne * -1
     
-  }
-  rect(30, yOne, 20, 20)
-  // console.log(yOne)
+  // }
+  // rect(30, yOne, 20, 20)
+  // // console.log(yOne)
 
-  yTwo = yTwo + speedTwo
-  if (yTwo < 0 || yTwo > height) {
-    speedTwo = speedTwo * -1
-  }
-  rect(60, yTwo, 20, 20)
+  // yTwo = yTwo + speedTwo
+  // if (yTwo < 0 || yTwo > height) {
+  //   speedTwo = speedTwo * -1
+  // }
+  // rect(60, yTwo, 20, 20)
 
-  yThree = yThree + speedThree
-  if (yThree < 0 || yThree > height) {
-    speedThree = speedThree * -1
-  }
-  rect(90, yThree, 20, 20)
+  // yThree = yThree + speedThree
+  // if (yThree < 0 || yThree > height) {
+  //   speedThree = speedThree * -1
+  // }
+  for (var i = 0; i < 3; i++) {
+    ys[i] = ys[i] + speeds[i]
+    if (ys[i] < 0 || ys[i] > height) {
+      speeds[i] = speeds[i] * -1
+    }
+  // rect(90, ys[i], 20, 20)
+  rect(30 * (i + 1), ys[i], 20, 20)
+}
 }
